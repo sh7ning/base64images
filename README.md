e36c7466ce73f13ac9719ca15c124914c3e2dd09
Yan Guangliang <yanguangliang@medatc.com>
2017-09-14 14:11:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # base64images
 Converts html img src to base64.
+
+## Usage
+
+See example html file which show how to snapshot web page.
+
+notice, you should open this page as a web site page, or you will get an error:`Access to Image at 'file//{{path}}/img/love.png' from origin 'null' has been blocked by CORS policy: Invalid response. Origin 'null' is therefore not allowed access.`
---
+++ base64images.js
@@ -0,0 +1,45 @@
+/**
+ * base64images.js
+ *
+ * @author sh7ning
+ * 2017.9.14
+ */
+function base64images(ele, callback) {
+    function getBase64Image(img) {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        var ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
+        return canvas.toDataURL("image/"+ext);
+    }
+
+    var length = $(ele).length;
+    var loadedImgs = 0;
+    function imgLoadPost() {
+        loadedImgs++;
+        if (loadedImgs == length) {
+            callback();
+        }
+    }
+
+    $(ele).each(function() {
+        var imgObj = new Image();
+        imgObj.setAttribute('crossOrigin', 'anonymous');
+        imgObj.src = $(this).attr('src');
+        imgObj.onload = function() {
+            if ($(this).attr('data-load')) { //avoid repeated loading
+                return;
+            }
+            var dataUrl = getBase64Image(imgObj);
+            //console.log(dataUrl);
+            $(this).attr('src', dataUrl);
+            $(this).attr('data-load', true)
+            imgLoadPost();
+        }.bind(this);
+        imgObj.onerror = function(){
+            imgLoadPost();
+        };
+    });
+}
